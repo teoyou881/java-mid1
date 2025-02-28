89f01a80d02c5269895b7676992d63fe65df13a2
Teo_You <teoyou881@gmail.com>
2025-02-28 16:33:16 -0500

    String class - basic

---
+++ src/lang/String/StringBasicMain.java
@@ -0,0 +1,13 @@
+package lang.String;
+
+public class StringBasicMain {
+
+  public static void main(String[] args) {
+
+    // There are 2 ways
+    String str1 = "hello";
+    String str2 = new String("hello");
+    System.out.println("str1 = " + str1);
+    System.out.println("str2 = " + str2);
+  }
+}
